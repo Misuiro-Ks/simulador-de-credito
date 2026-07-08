574a010a177c755758d5d62243221d33da4424aa
unknown <fabian.ni.fr@gmail.com>
2026-07-07 23:03:05 -0500

    Implementar calcularDisponible y función calcular

--- simulador.js
+++ simulador.js
@@ -1,1 +1,10 @@
 //AQUI EL JAVASCRIPT PARA MANIPULAR EL HTML
+
+function calcular() {
+    
+    let ingresos = recuperarFloat("txtIngresos");
+    let egresos = recuperarFloat("txtEgresos");
+
+    let disponible = calcularDisponible(ingresos, egresos);
+    document.getElementById("spnDisponible").textContent = "USD " + disponible.toFixed(2);
+}
